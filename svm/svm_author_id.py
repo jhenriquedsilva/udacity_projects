4425c6b4639a4439c1bb26d784dedb5f4b95a39b
jhenriquedsilva <josehs555@gmail.com>
2021-07-28 09:11:46 -0300

    Finishing lesson 2 - SVM discussion

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -9,30 +9,93 @@
 """
     
 import sys
+import numpy
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 from sklearn.svm import SVC
+
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-# I made the trainig dataset smaller for the algorithm to be faster
+"""
+# Using a linear kernel and the whole dataset
+# accuracy == 0.98
+
+clf = SVC(kernel='linear')
+clf.fit(features_train,labels_train)
+accuracy = clf.score(features_test,labels_test)
+print(accuracy)
+"""
+
+"""
+# Using a linear kernel and a smaller dataset
+features_train = features_train[:len(features_train)/100]
+labels_train = labels_train[:len(labels_train)/100]
+# Accuracy == 0.88
+
+clf = SVC(kernel='linear')
+clf.fit(features_train,labels_train)
+accuracy = clf.score(features_test,labels_test)
+print(accuracy)
+"""
+
+"""
+# Using a rbf kernel and a smaller dataset
+features_train = features_train[:len(features_train)/100]
+labels_train = labels_train[:len(labels_train)/100]
+# Accuracy == 0.61
+
+clf = SVC(kernel='rbf')
+clf.fit(features_train,labels_train)
+accuracy = clf.score(features_test,labels_test)
+print(accuracy)
+"""
+
+"""
+# Using a rbf kernel, a smaller dataset, and C equals 10000
+features_train = features_train[:len(features_train)/100]
+labels_train = labels_train[:len(labels_train)/100]
+# Accuracy == 0.89
+
+clf = SVC(kernel='rbf')
+clf.fit(features_train,labels_train)
+accuracy = clf.score(features_test,labels_test)
+print(accuracy)
+"""
+
+"""
+# Using a rbf kernel, the whole dataset, and C equals 10000
+# Accuracy == 0.99
+
+clf = SVC(kernel='rbf')
+clf.fit(features_train,labels_train)
+accuracy = clf.score(features_test,labels_test)
+print(accuracy)
+"""
+
+"""
+# Reducing the dataset size and predicting the author of some emails
 features_train = features_train[:len(features_train)/100]
 labels_train = labels_train[:len(labels_train)/100]
 
-# clf = SVC(kernel='linear') Best accuracy 0.9840728100113766 but it took a lot to train because I used the dataset completely
-# SVM is slower than naive_bayes
-# When the dataset is smaller, the accuracy is not high but the trainig time is much shorter
-# When C increases, more points are taaken into account and the accuracy is higher
-# With a high C, I end up with a more complex decision boundary
+clf = SVC(kernel='rbf', C=10000)
+clf.fit(features_train,labels_train)
+email1 = features_test[10]
+email2 = features_test[26]
+email3 = features_test[50]
+prediction = clf.predict([email1,email2,email3])
+print(prediction)
+"""
 
+# Using the whole dataset and identifying Chris' emails
+# {0: 881, 1: 877}
 
 clf = SVC(kernel='rbf', C=10000)
 clf.fit(features_train,labels_train)
-#accuracy = clf.score(features_test,labels_test)
-print(clf.predict([features_test[50]]))
-
-
+prediction = clf.predict(features_test)
+unique, counts = numpy.unique(prediction, return_counts=True)
+print(dict(zip(unique, counts)))
