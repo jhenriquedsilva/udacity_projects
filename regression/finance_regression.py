f042093f58783b304a66ac1e38e062c9760e9b3c
jhenriquedsilva <josehs555@gmail.com>
2021-08-05 10:04:02 -0300

    Finishing lesson 6 - Regression discussion

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -21,15 +21,15 @@ dictionary = pickle.load( open("../final_project/final_project_dataset_modified.
 
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
-features_list = ["bonus", "salary"]
+features_list = ["bonus", "long_term_incentive"]
 data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
 target, features = targetFeatureSplit( data )
 
 ### training-testing split needed in regression, just like classification
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -37,13 +37,29 @@ test_color = "b"
 ### Please name it reg, so that the plotting code below picks it up and 
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
+from sklearn.linear_model import LinearRegression
 
+"""
+# I used bonus against salary here and the result is really bad
+reg = LinearRegression()
+reg.fit(feature_train, target_train)
+print "The slope is",reg.coef_ # Output: 5.44
+print "The intercept is",reg.intercept_ # Output: -102360.54
+# R squared on training data
+print "R squared on training data is",reg.score(feature_train,target_train) # Output: 0.04550919269952436
+# R squared on test data
+print "R squared on test data is",reg.score(feature_test,target_test) # Output: -1.48499241736851
+"""
 
-
-
-
-
-
+# I used bonus against long_term_incentive here and the result is still bad
+reg = LinearRegression()
+reg.fit(feature_train, target_train)
+print "The slope is",reg.coef_ # Output: 1.19
+print "The intercept is",reg.intercept_ # Output: 554478.7562150093
+# R squared on training data
+print "R squared on training data is",reg.score(feature_train,target_train) # Output: 0.21708597125777662
+# R squared on test data
+print "R squared on test data is",reg.score(feature_test,target_test) # Output: -0.5927128999498639
 
 ### draw the scatterplot, with color-coded training and testing points
 import matplotlib.pyplot as plt
