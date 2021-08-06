da4c3d12c1078b24785d868978e498f83343a3a6
jhenriquedsilva <josehs555@gmail.com>
2021-08-06 10:21:35 -0300

    Finishing lesson 7 - Outliers discussion

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -12,9 +12,27 @@ def outlierCleaner(predictions, ages, net_worths):
     """
     
     cleaned_data = []
+    errors = []
+    print(predictions)
+    print(ages)
+    print(net_worths)
 
-    ### your code goes here
+    for prediction, net_worths in zip(predictions,net_worths):
+        error = net_worths - prediction
+        if error > 0:
+            errors.append(error)
+        elif error == 0:
+            errors.append(0)
+        else:
+            errors.append(-error)
 
+    counter = 0
+    while counter < len(errors) * 0.9:
+        index = errors.index(min(errors))
+        age = ages.pop(index)
+        net_worth = net_worth.pop(index)
+        error = errors.pop(index)
+        cleaned_data.append((age,net_worth,error))
     
     return cleaned_data
 
