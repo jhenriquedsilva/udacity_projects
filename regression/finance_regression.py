11f47a1ff09c9cc6644ef0ad7da6557a691c0aff
jhenriquedsilva <josehs555@gmail.com>
2021-08-06 09:01:27 -0300

    Finishing lesson 6 - Regression discussion

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -21,7 +21,7 @@ dictionary = pickle.load( open("../final_project/final_project_dataset_modified.
 
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
-features_list = ["bonus", "long_term_incentive"]
+features_list = ["bonus", "salary"]
 data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
 target, features = targetFeatureSplit( data )
 
@@ -39,7 +39,7 @@ test_color = "r"
 ### "r" to differentiate training points from test points.
 from sklearn.linear_model import LinearRegression
 
-"""
+
 # I used bonus against salary here and the result is really bad
 reg = LinearRegression()
 reg.fit(feature_train, target_train)
@@ -49,8 +49,9 @@ print "The intercept is",reg.intercept_ # Output: -102360.54
 print "R squared on training data is",reg.score(feature_train,target_train) # Output: 0.04550919269952436
 # R squared on test data
 print "R squared on test data is",reg.score(feature_test,target_test) # Output: -1.48499241736851
-"""
 
+
+"""
 # I used bonus against long_term_incentive here and the result is still bad
 reg = LinearRegression()
 reg.fit(feature_train, target_train)
@@ -60,6 +61,7 @@ print "The intercept is",reg.intercept_ # Output: 554478.7562150093
 print "R squared on training data is",reg.score(feature_train,target_train) # Output: 0.21708597125777662
 # R squared on test data
 print "R squared on test data is",reg.score(feature_test,target_test) # Output: -0.5927128999498639
+"""
 
 ### draw the scatterplot, with color-coded training and testing points
 import matplotlib.pyplot as plt
@@ -80,6 +82,15 @@ try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+
+reg.fit(feature_test, target_test)
+print "The slope is",reg.coef_ 
+print "The intercept is",reg.intercept_
+print "R squared on test data is",reg.score(feature_train,target_train)
+
+plt.plot(feature_train, reg.predict(feature_train), color="b")
+
+
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
