dc1c807de4b33d2233dc52561ccadadbfc4deae3
jhenriquedsilva <josehs555@gmail.com>
2021-08-08 11:05:13 -0300

    Finishing lesson 8 - Outliers discussion

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -9,19 +9,31 @@ from feature_format import featureFormat, targetFeatureSplit
 
 ### read in data dictionary, convert to numpy array
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
+data_dict.pop('TOTAL', None)
 features = ["salary", "bonus"]
 data = featureFormat(data_dict, features)
 
 
 ### your code below
-# Identifying the outlier
+# IDENTIFYING OUTLIERS
 
+
+# Identifying who has the highest salary
 highest = 0
 print type(highest)
 for name,features in data_dict.items():
     if (type(features['salary']) == int) and (features['salary'] > highest):
         print name
         highest = features['salary']
+
+print 
+
+#Identifying who has the highest bonus
+highest_bonus = 0
+for name,features in data_dict.items():
+    if (type(features['bonus']) == int) and (features['bonus'] > highest_bonus):
+        print name
+        highest_bonus = features['bonus']
 
 
 for point in data:
