3d068616e23ec7b61e475253595997aa0399fdb8
jhenriquedsilva <josehs555@gmail.com>
2021-08-19 10:33:57 -0300

    Finishing lesson 12 - Feature selection

--- feature_selection/find_signature.py
+++ feature_selection/find_signature.py
@@ -19,25 +19,39 @@ authors = pickle.load( open(authors_file, "r") )
 ### remainder go into training)
 ### feature matrices changed to dense representations for compatibility with
 ### classifier functions in versions 0.15.2 and earlier
-from sklearn import cross_validation
-features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(word_data, authors, test_size=0.1, random_state=42)
+from sklearn.model_selection import train_test_split
+features_train, features_test, labels_train, labels_test = train_test_split(word_data, authors, test_size=0.1, random_state=42)
 
 from sklearn.feature_extraction.text import TfidfVectorizer
-vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5,
-                             stop_words='english')
+vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5, 
+                                        stop_words='english')
 features_train = vectorizer.fit_transform(features_train)
 features_test  = vectorizer.transform(features_test).toarray()
-
 
 ### a classic way to overfit is to use a small number
 ### of data points and a large number of features;
 ### train on only 150 events to put ourselves in this regime
+
 features_train = features_train[:150].toarray()
 labels_train   = labels_train[:150]
 
-
+# features_test = features_test[:150]
+# labels_test = labels_test[:150]
 
 ### your code goes here
+from sklearn.tree import DecisionTreeClassifier
 
+clf = DecisionTreeClassifier()
+clf.fit(features_train,labels_train)
+# There was a problem over here. My accuracy is equal to 1.0 and I have no idea why this happening
+accuracy = clf.score(features_test,labels_test)
+print "Accuracy is", accuracy # Output: 1.0
 
+feature_importances = clf.feature_importances_
+counter = 0
+for feature_importance in feature_importances:
+    if feature_importance > 0.2:
+        print "Feature importance:", feature_importance
+    counter += 1
 
+print "Number of features:", len(feature_importances)
