2d0d91e567354200f6e17d90dd2bd429a0b396af
jhenriquedsilva <josehs555@gmail.com>
2021-08-23 12:05:27 -0300

    Finishing lesson 15 - Evaluation Metrics discussion

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -13,7 +13,11 @@
 import pickle
 import sys
 sys.path.append("../tools/")
+
 from feature_format import featureFormat, targetFeatureSplit
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import precision_score, recall_score
 
 data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
 
@@ -22,11 +26,18 @@ data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") 
 ### have a different feature list when you do the final project.
 features_list = ["poi", "salary"]
 
-data = featureFormat(data_dict, features_list)
+data = featureFormat(data_dict, features_list,sort_keys = '../tools/python2_lesson14_keys.pkl')
 labels, features = targetFeatureSplit(data)
-
+features_train,features_test,labels_train, labels_test = train_test_split(features,labels,random_state=42,test_size=0.3)
 
 
 ### it's all yours from here forward!  
-
-
+clf = DecisionTreeClassifier()
+# print labels_test
+# print float((len(labels_test) - 4)) / len(labels_test)
+clf.fit(features_train,labels_train)
+print clf.predict(features_test)
+accuracy = clf.score(features_test, labels_test)
+print accuracy
+print "Precision score: ", precision_score(labels_test, clf.predict(features_test))
+print "Recall score: ", recall_score(labels_test, clf.predict(features_test))
